b4b1854ba7b39c3350bf03c9c11370b3e0719af8
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-09-21 23:12:34 +0300

    Create Department

--- Company.Demo03.PL/Controllers/DepartmentController.cs
+++ Company.Demo03.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.BLL.Interfaces;
 using Company.BLL.Repositories;
+using Company.DAL.Models;
+using Company.Demo03.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Demo03.PL.Controllers
@@ -19,6 +21,36 @@ namespace Company.Demo03.PL.Controllers
         {
             var departments = _departmentRepository.GetAll();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) //Server Side Validation
+            {
+                var department = new Department()
+                { 
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+
+                var count = _departmentRepository.Add(department);
+
+                if (count>0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
         }
     }
 }
