4e5c52fc5a3929ab3ac806b4fb4c782cfc14c6f2
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-09-21 04:11:27 +0300

    Implementation Of DAL

---
+++ Company.DAL/Data/Contexts/CompanyDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.Contexts
+{
+    public class CompanyDBContext : DbContext
+    {
+        public CompanyDBContext(): base()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =.; Database = CompanyMVC; TrustedConnection = True; TrustServerCertificate = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
